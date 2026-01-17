e2ca975bf55cdaed9c360e200cde4cbc4acd632c
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2026-01-17 16:22:58 +0000

    ajout de fichiers de configuration Docker et .env, mise à jour des composants pour améliorer la gestion des utilisateurs

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,25 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8081",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '50mb',
-    },
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+				port: "8081",
+			},
+			{
+				protocol: "https",
+				hostname: new URL("https://admin.mec-ci.org/").hostname,
+			}
+		],
+	},
+	experimental: {
+		serverActions: {
+			bodySizeLimit: '50mb',
+		},
+	},
+	output: 'standalone',
 };
 
 export default withNextIntl(nextConfig);
